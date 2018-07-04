8c7c28d41d87048688ce090b8e945f1c216641de
nmsbane <madhusudhan638@gmail.com>
2018-07-04 21:57:41 +0530

    set up docker for development environment also run flask app inside the docker container

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,12 +1,16 @@
 # services/users/project/__init__.py
+import os
 
 from flask import Flask, jsonify
 #from .config import DevelopmentConfig
+
 # instantiate the app
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
 #app.config.from_object(DevelopmentConfig)
 
 @app.route('/users/ping', methods=['GET'])
