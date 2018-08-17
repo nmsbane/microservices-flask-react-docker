315b547171ae9af1e61800baf792f4a8f8151a1c
nmsbane <madhusudhan638@gmail.com>
2018-08-17 20:35:34 +0530

    added testcafe e2e tests for register, login and logout

--- services/client/src/components/__tests__/App.test.jsx
+++ services/client/src/components/__tests__/App.test.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-
+import { shallow, mount } from 'enzyme';
+import { MemoryRouter as Router } from 'react-router-dom';
 import App from '../../App';
+
+beforeAll(() => {
+  global.localStorage = {
+    getItem: () => {
+      return 'sometoken'
+    }
+  }
+});
 
 test('App renders without crashing', () => {
   const wrapper = shallow(<App />, {disableLifecycleMethods:true});
